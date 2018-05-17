11b5ae58f02076592936dc0d179a19cbbc201a09
Reinaldo <reinaldojunior@trii.com.br>
2018-05-17 15:34:42 -0300

    SelectRepo

---
+++ src/components/Logo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Logo extends Component{
+	render(){
+		return(
+			<div className="logo">
+				GitChart
+				<div className="com">.com</div>
+			</div>
+		);
+	}
+}
+
+export default Logo;
