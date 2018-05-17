2ea644aad7df89e4f277816899b6ba7d442e7ee8
Juuhz <juuhz.easy@gmail.com>
2018-05-17 07:16:47 -0300

    estrutura

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import SelectRepo 			from './SelectRepo';
+import Infos 				from './Infos';
+import Chart				from './Chart';
+
+class App extends Component{
+	render(){
+		return (
+			<div>
+				{/*<SelectRepo />
+				<Infos />
+        		<Chart />*/}
+      		</div>		
+		);
+	}
+}
+
+export default App;
